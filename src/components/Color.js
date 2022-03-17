19b77dda07ce3bc874784cd413c8b9673a525d56
navidabdi <artsanaat@gmail.com>
2022-03-17 16:38:49 +0330

    add the normal palette data

---
+++ src/components/Color.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Color = () => {
+  return (
+    <div className="color">
+      <p>Color Name</p>
+    </div>
+  );
+};
+
+export default Color;
---
+++ src/data.js
@@ -0,0 +1,108 @@
+import React from 'react';
+
+const data = () => {
+  return [
+    {
+      id: 1,
+      name: 'WET ASPHALT',
+      color: '#34495e',
+    },
+    {
+      id: 2,
+      name: 'AMETHYST',
+      color: '#9b59b6',
+    },
+    {
+      id: 3,
+      name: 'PETER RIVER',
+      color: '#3498db',
+    },
+    {
+      id: 4,
+      name: 'EMERALD',
+      color: '#2ecc71',
+    },
+    {
+      id: 5,
+      name: 'TURQUOISE',
+      color: '#1abc9c',
+    },
+    {
+      id: 6,
+      name: 'MIDNIGHT BLUE',
+      color: '#2c3e50',
+    },
+    {
+      id: 7,
+      name: 'WISTERIA',
+      color: '#8e44ad',
+    },
+    {
+      id: 8,
+      name: 'BELIZE HOLE',
+      color: '#2980b9',
+    },
+    {
+      id: 9,
+      name: 'NEPHRITIS',
+      color: '#27ae60',
+    },
+    {
+      id: 10,
+      name: 'GREEN SEA',
+      color: '#16a085',
+    },
+    {
+      id: 11,
+      name: 'CONCRETE',
+      color: '#95a5a6',
+    },
+    {
+      id: 12,
+      name: 'CLOUDS',
+      color: '#ecf0f1',
+    },
+    {
+      id: 13,
+      name: 'ALIZARIN',
+      color: '#e74c3c',
+    },
+    {
+      id: 14,
+      name: 'CARROT',
+      color: '#e67e22',
+    },
+    {
+      id: 15,
+      name: 'SUN FLOWER',
+      color: '#f1c40f',
+    },
+    {
+      id: 16,
+      name: 'ASBESTOS',
+      color: '#7f8c8d',
+    },
+    {
+      id: 17,
+      name: 'SILVER',
+      color: '#bdc3c7',
+    },
+    {
+      id: 18,
+      name: 'POMEGRANATE',
+      color: '#c0392b',
+    },
+    {
+      id: 19,
+      name: 'PUMPKIN',
+      color: '#d35400',
+    },
+    {
+      id: 20,
+      name: 'ORANGE',
+      color: '#f39c12',
+    },
+  ];
+};
+
+export default data;
